1ad1b0e09530f17370b3fff8736150a2641c0729
Lukasz Adamczyk <ladamczyk.it@gmail.com>
2024-08-20 21:13:15 +0200

    chore: qf to tools

--- packages/eslint-v9-js/src/tools.ts
+++ packages/eslint-v9-js/src/tools.ts
@@ -6,7 +6,9 @@ import type { Linter } from 'eslint';
 export const omitRules = (sourceConfig: Linter.Config, rulesToOmit: string[]): Linter.Config => {
   const newConfig = merge({}, sourceConfig);
 
-  return omit(newConfig.rules, rulesToOmit);
+  newConfig.rules = omit(newConfig.rules, rulesToOmit);
+
+  return newConfig;
 };
 
 export const omitRulesForConfigCollection = (
