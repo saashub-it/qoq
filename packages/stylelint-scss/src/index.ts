ed0911af29a394cee5cdfd5c21eca488f69045ca
Łukasz Adamczyk <ladamczyk-it@gmail.com>
2025-03-07 09:23:49 +0100

    chore: stylelint scss adjust

--- packages/stylelint-scss/src/index.ts
+++ packages/stylelint-scss/src/index.ts
@@ -4,4 +4,18 @@ import { Config } from 'stylelint';
 export const baseConfig: Config = {
   ...baseCssConfig,
   extends: ['stylelint-config-standard-scss', 'stylelint-prettier'],
+  overrides: [
+    {
+      files: ['*.scss', '**/*.scss'],
+      rules: {
+        'plugin/no-unsupported-browser-features': [
+          true,
+          {
+            severity: 'warning',
+            ignore: ['css-nesting'],
+          },
+        ],
+      },
+    },
+  ],
 };
