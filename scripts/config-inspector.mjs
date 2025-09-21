4ee07082d9c30ebdb98764bc40cbf450cf67fdf6
Lukasz Adamczyk <ladamczyk.it@gmail.com>
2025-09-21 16:27:13 +0200

    chore: adjust

--- scripts/config-inspector.mjs
+++ scripts/config-inspector.mjs
@@ -1,8 +1,10 @@
 import { spawn } from 'child_process';
 import { parse } from 'node:path';
+import { readdirSync, existsSync } from 'node:fs'
 
 const paths = [];
-const getModifiedFiles = async () => new Promise((resolve, reject) => {
+const getModifiedFiles = async () =>
+  new Promise((resolve, reject) => {
     const child = spawn('git ls-files', ['--modified'], { shell: true });
 
     child.stdout.on('data', (data) => paths.push(...data.toString('utf-8').split('\n')));
@@ -12,47 +14,59 @@ const getModifiedFiles = async () => new Promise((resolve, reject) => {
     });
 
     child.on('close', (code) => {
-          resolve(code ?? 0);
-        });
-})
+      resolve(code ?? 0);
+    });
+  });
 
 await getModifiedFiles();
 
-const packagesToCheck = paths.filter((path) => !!path).reduce((acc, current) => {
+const allPackages = readdirSync('./packages').filter((path) => path.startsWith('eslint-v9-'));
+const emptyPackages = allPackages.filter((packageName) => !existsSync(`./packages/${packageName}/stats`));
+
+const packagesToCheck = paths
+  .filter((path) => !!path)
+  .reduce((acc, current) => {
     const path = parse(current);
 
     if (path.dir.startsWith('packages/eslint-v9-')) {
-        const [,packageName] = path.dir.split('/')
+      const [, packageName] = path.dir.split('/');
 
-        if (!acc.includes(packageName)) {
-            acc.push(packageName)
-        }
+      if (!acc.includes(packageName)) {
+        acc.push(packageName);
+      }
     }
 
     return acc;
-}, []);
+  }, []);
 
-const executeInspector = async (packageName) => new Promise((resolve, reject) => {
-    const child = spawn('npx -y @eslint/config-inspector build', [`--config=packages/${packageName}/eslint.config.local.js`, `--outDir=packages/${packageName}/stats`], { shell: true });
-    
+const executeInspector = async (packageName) =>
+  new Promise((resolve, reject) => {
+    const child = spawn(
+      'npx -y @eslint/config-inspector build',
+      [
+        `--config=packages/${packageName}/eslint.config.local.js`,
+        `--outDir=packages/${packageName}/stats`,
+      ],
+      { shell: true }
+    );
+
     child.stdout.on('data', (data) => {
-          process.stdout.write(data.toString('utf-8'));
-        });
-    
-        child.stderr.on('data', (data) => {
-          process.stderr.write(data.toString('utf-8'));
-        });
-    
-        child.on('error', (error) => {
-          reject(error);
-        });
-    
-        child.on('close', (code) => {
-          resolve(code ?? 0);
-        });
-})
+      process.stdout.write(data.toString('utf-8'));
+    });
 
-for (const packageName of packagesToCheck) {
-    await executeInspector(packageName);
+    child.stderr.on('data', (data) => {
+      process.stderr.write(data.toString('utf-8'));
+    });
+
+    child.on('error', (error) => {
+      reject(error);
+    });
+
+    child.on('close', (code) => {
+      resolve(code ?? 0);
+    });
+  });
+
+for (const packageName of new Set(emptyPackages.concat(packagesToCheck))) {
+  await executeInspector(packageName);
 }
-
